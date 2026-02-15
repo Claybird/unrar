a80c3e3644c1be56f34f88f519c2de2b8ee3ad6a
Claybird <claybird.without.wing@gmail.com>
2026-02-16 07:52:01 +0900

    updated to 7.2.4 with WINVER / _WIN32_WINNT fixes

--- version.hpp
+++ version.hpp
@@ -1,6 +1,6 @@
-#define RARVER_MAJOR     6
-#define RARVER_MINOR    22
+#define RARVER_MAJOR     7
+#define RARVER_MINOR    20
 #define RARVER_BETA      0
-#define RARVER_DAY      29
-#define RARVER_MONTH     5
-#define RARVER_YEAR   2023
+#define RARVER_DAY       1
+#define RARVER_MONTH     2
+#define RARVER_YEAR   2026
